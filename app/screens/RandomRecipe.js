9d1f73e5fa55ea9169923a823c8fd5ce6984a964
Lee <hangsen.lee@hexainitiative.com>
2021-03-17 13:31:50 +0800

    added randdom recipe

---
+++ app/screens/RandomRecipe.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const RandomRecipe = () => {
+  return (
+    <View>
+      <Text>random recipe</Text>
+    </View>
+  );
+};
+
+export default RandomRecipe;
+
+const styles = StyleSheet.create({});
